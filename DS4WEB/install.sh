cfc4b99601d10dd5bd02ceef1e372fd860a76303
wertiop1 <102436461+wertiop1@users.noreply.github.com>
2026-03-25 14:11:23 -0700

    added calculator

--- DS4WEB/install.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-wget https://raw.githubusercontent.com/devpotatoes/DS4WEB/main/dist/DS4WEB.min.js
-wget https://raw.githubusercontent.com/devpotatoes/DS4WEB/main/dist/DS4WEB.wasm
-wget https://raw.githubusercontent.com/devpotatoes/DS4WEB/main/dist/DS4WEB.js
